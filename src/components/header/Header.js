46b59e97b9a9256041500eac6e6022db26299929
minsun <minn602@gmail.com>
2025-03-25 19:38:50 +0000

    fix: header and footer ui

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,24 @@
-import { useNavigate } from "react-router";
+import { useNavigate, useLocation } from "react-router";
+
 const Header = () => {
   const navigation = useNavigate();
+  const location = useLocation();
+
+  const isHome = location.pathname === "/";
 
   return (
-    <header className="w-full fixed top-0 left-0 flex z-10">
+    <header className="w-full fixed top-0 left-0 p-3 flex items-center z-10">
+      {!isHome && (
+        <img
+          onClick={() => navigation("/")}
+          className="w-[36px] h-[36px] cursor-pointer"
+          src="/~mj469/assets/back.png"
+          alt="back"
+        />
+      )}
       <div
-        onClick={() => navigation("/~mj469")}
-        className="pt-4 m-auto flex items-center cursor-pointer"
+        onClick={() => navigation("/")}
+        className="m-auto flex items-center cursor-pointer"
       >
         <img className="w-[36px]" src="/~mj469/assets/cherry.png" alt="logo" />
         <h1 className="font-playwrite text-[36px] text-primary">Desserts</h1>
